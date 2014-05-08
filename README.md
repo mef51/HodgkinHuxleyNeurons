8a1196e977b6b64870b74bd9f14b03dd194cc862
mohammed chamma <hms.mohammed@gmail.com>
2014-05-08 18:11:29 -0400

    implement most of model, add plots, add readme

---
+++ README.md
@@ -0,0 +1,17 @@
+Hodgkin Huxley Model of Neuron Signals
+=====================================
+
+Hodgkin and Huxley's original paper (1952): http://www.ncbi.nlm.nih.gov/pmc/articles/PMC1392413/pdf/jphysiol01442-0106.pdf
+
+This was the first evidence of ion channels in cell membranes. Ion channels can control the voltage
+across the membrane by passing charged ions (ex. sodium, potassium) through the cell wall. When a signal
+travels down a neuron it's by a concerted effort of the ion channels in the membrane, opening and closing in such a way
+that the voltage signal travels down the axon of a neuron until it reaches another neuron.
+
+Todo
+=====
+* Fix the model. It plots but the plot looks kinda weird so one of the equations must be wrong
+
+
+![](model.jpg)
+![](mhn.jpg)
--- model.py
+++ model.py
@@ -13,7 +13,7 @@ nInf   = lambda v: alphaN(v)/(alphaN(v) + betaN(v))
 
 # Sodium (Na) Channel (activating)
 alphaM = np.vectorize(lambda v: 0.1*(25-v) / (np.exp((25-v)/10) - 1) if v!= 25 else 1)
-betaM = lambda v: 4* np.exp(-v/18)
+betaM = lambda v: 4 * np.exp(-v/18)
 mInf = lambda v: alphaM(v)/(alphaM(v) + betaM(v))
 
 # Sodium (Na) Channel (inactivating)
@@ -22,4 +22,64 @@ betaH  = lambda v: 1/(np.exp((30-v)/10) + 1)
 hInf   = lambda v: alphaH(v)/(alphaH(v) + betaH(v))
 
 # Channel Activity
-v = np.arangy(-50, 151) # millivolts
+v = np.arange(-50, 151) # millivolts
+pylab.figure()
+pylab.plot(v, mInf(v), v, hInf(v), v, nInf(v))
+pylab.legend(('m', 'h', 'n'))
+pylab.title('Steady state values of ion channel gating variables')
+pylab.ylabel('Magnitude')
+pylab.xlabel('Voltage (mV)')
+pylab.savefig("mhn.jpg")
+
+# Setup parameters and state variables
+T    = 55    # ms
+dt   = 0.025 # ms
+time = np.arange(0, T+dt, dt)
+
+# Hodgkin-Huxley Parametahs (from the papah!)
+restingVoltage     = 0      # V_rest (mv)
+Cm                 = 1      # uF/cm2
+gBarNa             = 120    # mS/cm2
+gBarK              = 3      # mS/cm2
+gBarL              = 0.3    # mS/cm2
+sodiumPotential    = 115    # mV
+potassiumPotential = -12    # mv
+leakagePotential   = 10.613 # mV
+
+Vm    = np.zeros(len(time)) # The membrane potential we wanna find
+Vm[0] = restingVoltage
+m     = mInf(restingVoltage)
+h     = hInf(restingVoltage)
+n     = nInf(restingVoltage)
+
+# Current Stimulus
+# This is like a pulse
+I = np.zeros(len(time))
+for i, t in enumerate(time):
+    if 5 <= t <= 30:
+        I[i] = 10 # uA/cm2
+
+# Main loop
+for i in range(1, len(time)):
+    sodiumConductance    = gBarNa * (m**3) * h
+    potassiumConductance = gBarK  * (n**4)
+    leakageConductance   = gBarL
+
+    # integrate the equations on m, h, and n
+    m += (alphaM(Vm[i-1]) * (1 - m) - betaM(Vm[i-1])*m) * dt
+    h += (alphaH(Vm[i-1]) * (1 - h) - betaH(Vm[i-1])*h) * dt
+    n += (alphaN(Vm[i-1]) * (1 - n) - betaN(Vm[i-1])*n) * dt
+
+    # now integrate the changes in V
+    sodiumCurrent = sodiumConductance * (Vm[i-1] - sodiumPotential)
+    potassiumCurrent = potassiumConductance * (Vm[i-1] - potassiumPotential)
+    leakageCurrent = leakageConductance * (Vm[i-1] - leakagePotential)
+    Vm[i] = Vm[i-1] + (I[i-1] - sodiumCurrent - potassiumCurrent - leakageCurrent) * dt / Cm
+
+pylab.figure()
+pylab.plot(time, Vm, time, -30+I)
+pylab.title('Hodgkin-Huxley Example')
+pylab.ylabel('Membrane Potential (mV)')
+pylab.xlabel('Time (msec)')
+pylab.show()
+pylab.savefig("model.jpg")
